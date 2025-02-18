a39e962ab026edd7cd14fc011702c709a28e2567
naerDD <806915230@qq.com>
2025-02-18 17:13:08 +0800

    0218 17:12改用并发 速度并没有提升 短板效应

--- src/main/java/com/naer/controller/SearchController.java
+++ src/main/java/com/naer/controller/SearchController.java
@@ -5,14 +5,10 @@ import com.naer.common.BaseResponse;
 import com.naer.common.ErrorCode;
 import com.naer.common.ResultUtils;
 import com.naer.exception.BusinessException;
-import com.naer.exception.ThrowUtils;
-import com.naer.model.dto.picture.PicturteQueryRequest;
 import com.naer.model.dto.post.PostQueryRequest;
 import com.naer.model.dto.search.SearchRequest;
 import com.naer.model.dto.user.UserQueryRequest;
 import com.naer.model.entity.Picture;
-import com.naer.model.entity.Post;
-import com.naer.model.entity.User;
 import com.naer.model.vo.PostVO;
 import com.naer.model.vo.SearchVo;
 import com.naer.model.vo.UserVO;
@@ -20,18 +16,14 @@ import com.naer.service.PictureService;
 import com.naer.service.PostService;
 import com.naer.service.UserService;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.search.SearchService;
-import org.springframework.stereotype.Controller;
+import me.chanjar.weixin.mp.bean.card.enums.BusinessServiceType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 
@@ -59,21 +51,40 @@ public class SearchController {
     @PostMapping("/all")
     public BaseResponse<SearchVo> SearchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request) {
 
-            String searchText = searchRequest.getSearchText();
-            Page<Picture> picturePage = pictureService.listPictures(searchText, 1, 10);
+        String searchText = searchRequest.getSearchText();
 
+
+        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(()->{
             UserQueryRequest userQueryRequest = new UserQueryRequest();
             userQueryRequest.setUserName(searchText);
             Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
+            return  userVOPage;
+        });
 
+        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(()->{
             PostQueryRequest postQueryRequest = new PostQueryRequest();
             postQueryRequest.setSearchText(searchText);
             Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
+            return postVOPage;
+        });
 
+        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(()->{
+            Page<Picture> picturePage = pictureService.listPictures(searchText, 1, 10);
+            return  picturePage;
+        });
+
+        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
+        try {
             SearchVo searchVo = new SearchVo();
+            Page<UserVO> userVOPage = userTask.get();
+            Page<Picture> picturePage = pictureTask.get();
+            Page<PostVO> postVOPage = postTask.get();
+            searchVo.setUserList(userVOPage.getRecords());
             searchVo.setPictureVOList(picturePage.getRecords());
             searchVo.setPostList(postVOPage.getRecords());
-            searchVo.setUserList(userVOPage.getRecords());
             return ResultUtils.success(searchVo);
+        }catch (Exception e){
+            throw  new BusinessException(ErrorCode.SYSTEM_ERROR,"查询异常");
+        }
     }
 }
